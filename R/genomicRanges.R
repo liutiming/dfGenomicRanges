2656cbabc6b833b4d6763ab1442d7625eaf9207a
Timing Liu <liutiming@outlook.com>
2021-04-14 14:45:33 +0100

    first commit

---
+++ R/genomicRanges.R
@@ -0,0 +1,13 @@
+#' @export
+tibble_makeGenomicRanges  <- function(tbl) {
+  tbl  %>%
+    GenomicRanges::makeGRangesFromDataFrame(start.field = "pos", end.field = "pos", keep.extra.columns = TRUE)
+}
+
+#' @export
+tibble_subsetByOverlaps <- function(range_1, range_2) {
+  IRanges::subsetByOverlaps(range_1, range_2)  %>%
+    tibble::as_tibble()  %>%
+    dplyr::rename("chr" = "seqnames", "pos" = "start")  %>%
+    dplyr::select(chr, pos, end)
+}
