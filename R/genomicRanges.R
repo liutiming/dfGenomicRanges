2e48eceee40d03c703a6f41959ca0a545d612687
Timing Liu <liutiming@outlook.com>
2021-04-15 07:45:45 +0100

    mutate_chr()

--- R/genomicRanges.R
+++ R/genomicRanges.R
@@ -11,3 +11,9 @@ tibble_subsetByOverlaps <- function(range_1, range_2) {
     dplyr::rename("chr" = "seqnames", "pos" = "start")  %>%
     dplyr::select(chr, pos, end)
 }
+
+#' @export
+mutate_chr  <- function(tbl) {
+  result <- tbl %>% mutate(chr = str_c("chr", chr))
+  return(result)
+}
