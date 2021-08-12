8f01ae47fdb9cfbb1b0080ec8c9aff0998870120
Timing Liu <liutiming@outlook.com>
2021-08-12 11:50:16 +0100

    add plink2_bfile_general

--- R/system.R
+++ R/system.R
@@ -9,3 +9,15 @@ write_chr_tbl <- function(chr_tbl, prefix, suffix, delim, col_names) {
   vroom::vroom_write(chr_tbl, path = file_name, delim = delim, col_names = col_names)
   return(file_name)
 }
+
+#' plink 2 general command
+#' @export
+
+plink2_bfile_general <- function(input, chr, prefix, argument) {
+  bfile  <- stringr::str_replace(input, ".bed", "") %>%
+    stringr::str_replace(".bim", "")
+  command <- glue::glue("plink2 --threads 16 --bfile {bfile} {argument} --make-bed --out {prefix}{chr}")
+  system(command)
+  output <- glue::glue("{prefix}{chr}.bim")
+  return(output)
+}
